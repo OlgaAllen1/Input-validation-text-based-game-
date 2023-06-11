1a1153c81f5af51401a697057f86ccbc7d1d28de
Olga Allen <32139467+OlgaAllen1@users.noreply.github.com>
2023-06-11 11:14:44 -0600

    commit 2

--- Input validation (text based game).cpp
+++ Input validation (text based game).cpp
@@ -1,10 +1,22 @@
 using namespace std;
 #include <iostream>
 string input (string prompt, string validResponse1, string validResponse2) {
-
+	string userInput;
+	while (true) {
+		cout << prompt;
+		cin >> userInput;
+		if (userInput == validResponse1 || userInput == validResponse2) {
+			break;
+		}
+		else {
+			cout << "Invalide response.";
+		}
+	}
+	return userInput;
 }
 
 int main()
 {
-   
+	string response = input("Go to the forest or the beach: ", "forest", "beach");
+	cout << "You choose to go to the " << response << "." << endl;
 }
